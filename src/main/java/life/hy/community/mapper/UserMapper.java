7e2207cec08c153f036af68d0c6143fee587f0c2
FromSouthToNorth <598050554@qq.com>
2020-01-19 16:28:45 +0800

    add check cookies

--- src/main/java/life/hy/community/mapper/UserMapper.java
+++ src/main/java/life/hy/community/mapper/UserMapper.java
@@ -3,9 +3,16 @@ package life.hy.community.mapper;
 import life.hy.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface UserMapper {
     @Insert("INSERT INTO `user` (name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
